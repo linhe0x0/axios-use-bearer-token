baf99a62cfd2efbc1d4d346d604adc08b1bba8a4
sqrtthree <imsqrtthree@gmail.com>
2021-07-17 18:37:45 +0800

    feat: add support for pkg.module and export umd file

---
+++ dist/index.umd.js
@@ -0,0 +1,33 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.useBearerToken = factory());
+}(this, (function () { 'use strict';
+
+  function bearerToken(options = {}) {
+    const { bearerToken } = options;
+
+    if (!bearerToken) {
+      throw Error('barerToken is required.')
+    }
+
+    return async function interceptor(config) {
+      let accessToken = '';
+
+      if (typeof bearerToken === 'string') {
+        accessToken = bearerToken;
+      } else {
+        accessToken = await getAccessToken();
+      }
+
+      if (accessToken) {
+        config.headers['Authorization'] = `Bearer ${accessToken}`;
+      }
+
+      return config
+    }
+  }
+
+  return bearerToken;
+
+})));
